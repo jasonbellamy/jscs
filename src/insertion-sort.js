b6576a3bf594520d6672c1bc58c2e3d841ca7648
Jason Bellamy <j@sonbellamy.com>
2016-08-18 23:39:38 -0400

    Update insertionSort to skip the first item (a list of 1 is always sorted)

--- src/insertion-sort.js
+++ src/insertion-sort.js
@@ -6,7 +6,7 @@
  * @param {Array} xs the list to sort
  */
 export function insertionSort(xs) {
-  for (let i = 0; i < xs.length; i++) {
+  for (let i = 1; i < xs.length; i++) {
     for (let j = 0; j < i; j++) {
       if(xs[i] < xs[j]) {
         xs.splice(j, 0, xs.splice(i, 1)[0]);
